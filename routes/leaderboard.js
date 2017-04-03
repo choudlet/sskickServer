3d3dba59aa02b15975a3beeca842ae4db204412d
Chris Houdlette <choudlette@gmail.com>
2017-04-03 15:45:56 -0600

    data sent by client, now setting up now sequelize model

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 const modelConnect = require('../sequelize/controllers')
 
 router.post('/', (req,res,next)=>{
   console.log(req.body);
+  let trainingRecord = req.body.data;
+  trainingRecord.minutes = Number(trainingRecord.minutes)
+  trainingRecord.seconds = Number(trainingRecord.seconds)
+  let trainingTime = moment().minute(trainingRecord.minutes).second(trainingRecord.seconds)
+  
 })
 
 module.exports = router;
